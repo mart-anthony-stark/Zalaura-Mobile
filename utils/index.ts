181087725152f9f9f936bd78aa2b7704400717ef
mart-anthony-stark <martsalazar0@gmail.com>
2024-03-08 10:22:30 +0800

    Add: apply discount

--- utils/index.ts
+++ utils/index.ts
@@ -2,3 +2,7 @@ export const currencyFormatter = new Intl.NumberFormat("en-US", {
   style: "currency",
   currency: "USD",
 });
+
+export const applyDiscount = (price: number, discount: number): number => {
+  return ((price) * (1 - discount))
+};
