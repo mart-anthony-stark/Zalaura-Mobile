66a673240af263682a1386fa2afb1b0fbd14c4f3
mart-anthony-stark <martsalazar0@gmail.com>
2024-03-08 10:22:20 +0800

    Add: add to cart

--- app/products/[id]/index.tsx
+++ app/products/[id]/index.tsx
@@ -1,15 +1,29 @@
 import { router, useLocalSearchParams } from "expo-router";
 import { useEffect, useState } from "react";
-import { Image, ScrollView, Text, View } from "react-native";
+import {
+  Image,
+  ScrollView,
+  Text,
+  TouchableHighlight,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { getOneProduct } from "../../../services/product.service";
 import { Product } from "../../../libs/types";
-import { currencyFormatter } from "../../../utils";
+import { applyDiscount, currencyFormatter } from "../../../utils";
 import Loading from "../../../components/Loading";
 
 const SingleProduct = () => {
   const { id } = useLocalSearchParams();
   const [product, setProduct] = useState<Product>();
   const [isLoading, setIsLoading] = useState(true);
+  const [itemCount, setItemCount] = useState(0);
+
+  const handleItemCount = (operator: "add" | "subtract"): void => {
+    if (operator === "add") setItemCount(itemCount + 1);
+    else if (operator === "subtract" && itemCount > 0)
+      setItemCount(itemCount - 1);
+  };
 
   const handleGetProduct = async () => {
     const response = await getOneProduct(String(id));
@@ -19,6 +33,7 @@ const SingleProduct = () => {
     setProduct(response?.data);
     setIsLoading(false);
   };
+
   useEffect(() => {
     handleGetProduct();
   }, []);
@@ -27,20 +42,63 @@ const SingleProduct = () => {
 
   return (
     <ScrollView className="flex-1 bg-white">
-    <View className="flex-1 items-start gap-4 p-2">
+      <View className="flex-1 items-start gap-4 p-2">
         <Image
-          className="h-[350] w-[300] my-2 self-center"
+          className="h-[350] w-[300] mb-2 self-center"
           source={{
             uri: product?.image,
           }}
         />
         <Text className="font-bold text-3xl">{product?.title}</Text>
-        <Text className="font-bold text-xl">{product?.category?.toUpperCase()}</Text>
-        <Text>{product?.description}</Text>
-        <Text className="text-red-500 px-2 rounded-full border border-red-500 w-fit text-4xl">
-          {currencyFormatter.format(product!.price)}
+        <Text className="font-bold text-xl">
+          {product?.category?.toUpperCase()}
         </Text>
-    </View>
+
+        <View className="self-stretch flex-row justify-between">
+          <View>
+            <Text className="text-red-500 mr-8 px-2 rounded-full border border-red-500 w-fit text-2xl">
+              {currencyFormatter.format(applyDiscount(product!.price, 0.2))}
+            </Text>
+            <Text className="text-gray-500 line-through mr-8 px-2 rounded-full w-fit text-lg">
+              {currencyFormatter.format(product!.price)}
+            </Text>
+          </View>
+
+          <View className="">
+            <View className="flex-row">
+              <TouchableHighlight onPress={() => handleItemCount("subtract")}>
+                <View className="border bg-white w-12 h-8 justify-center items-center">
+                  <Text className="text-4xl">-</Text>
+                </View>
+              </TouchableHighlight>
+
+              <Text className="text-2xl mx-4">{itemCount}</Text>
+
+              <TouchableHighlight onPress={() => handleItemCount("add")}>
+                <View className="border bg-white w-12 h-8 justify-center items-center">
+                  <Text className="text-2xl">+</Text>
+                </View>
+              </TouchableHighlight>
+            </View>
+
+            <TouchableOpacity>
+              <View className="bg-black mt-2 p-2 items-center">
+                <Text className="text-white">Add to Cart</Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+        </View>
+
+        <Text>{product?.description}</Text>
+
+        <Text>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio
+          repudiandae recusandae illo doloribus nesciunt autem facere rerum odit
+          quam officiis iste, ratione, alias quibusdam debitis molestiae cumque
+          provident modi a. Dolor, voluptatem rem? Eius expedita voluptatibus
+          hic similique, excepturi tempora.
+        </Text>
+      </View>
     </ScrollView>
   );
 };
