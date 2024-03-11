50059d26417120debb672d670ee968f313a2328e
mart-anthony-stark <martsalazar0@gmail.com>
2024-03-11 08:50:40 +0800

    Add: error handling for getting product

--- services/product.service.ts
+++ services/product.service.ts
@@ -1,19 +1,22 @@
+import { ToastAndroid } from "react-native";
 import { makeRequest } from "../utils/axios";
 
 export const getProducts = async () => {
   try {
     const data = await makeRequest.get("/products");
-    return data
+    return data;
   } catch (error) {
-    console.log(error)
+    ToastAndroid.show("Somewthing went wrong!", ToastAndroid.BOTTOM);
+    console.log(error);
   }
 };
 
 export const getOneProduct = async (id: string) => {
-    try {
-        const data = await makeRequest.get(`/products/${id}`);
-        return data
-      } catch (error) {
-        console.log(error)
-      }
-}
+  try {
+    const data = await makeRequest.get(`/products/${id}`);
+    return data;
+  } catch (error) {
+    ToastAndroid.show("Somewthing went wrong!", ToastAndroid.BOTTOM);
+    console.log(error);
+  }
+};
